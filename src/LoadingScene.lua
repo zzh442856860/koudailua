4097b828b9397d2289edbe938c4f658c7f55f1b2
nike <zzh442856860@163.com>
2014-05-17 22:58:42 +0800

    add res

---
+++ src/LoadingScene.lua
@@ -0,0 +1,76 @@
+--zzh 2014-05-17
+
+require "ResourceName"
+
+local layer = nil
+local bg = nil
+local progressBg = nil
+local progress = nil
+
+local s = 0
+local w = 0
+local h = 0
+
+local function init() --@return typeOrObject
+    layer = cc.LayerColor:create(cc.c4b(0,0,0,0))
+    
+    s = cc.Director:getInstance():getWinSize()
+    w = s.width/2
+    h = s.height/2
+    --±³¾°
+    bg = cc.Sprite:create(M_LOADING_BG)
+    bg:setPosition(w, h)
+    layer:addChild(bg, 0)
+    --
+    progressBg = cc.Sprite:create(M_PROGRESS_BG)
+    progressBg:setPosition(w,h)
+    layer:addChild(progressBg, 1)
+    --
+    progress = cc.ProgressTimer:create(cc.Sprite:create(M_PROGRESS))
+    progress:setType(cc.PROGRESS_TIMER_TYPE_BAR)
+    progress:setMidpoint(cc.p(1,0))
+    progress:setBarChangeRate(cc.p(1,0))
+    progress:setPosition(w,h)
+    layer:addChild(progress, 2)
+    	
+end
+
+local function textureLoaded(pObj)
+    local name = pObj
+    cclog("Image loaded: ", name)
+end 
+
+local function loadTexture() 
+    local cache = cc.Director:getInstance():getTextureCache()
+    cache:addImageAsynv(M_LOGIN_BG, textureLoaded)
+    cache:addImageAsynv(M_LOGIN_BTN_UP, textureLoaded)
+    cache:addImageAsynv(M_LOGIN_BTN_DOWN, textureLoaded)
+    cache:addImageAsynv(M_CHUANGJIAN_UP, textureLoaded)
+end
+
+local function onExit() --@return typeOrObject
+	cc.Director:getInstance():getTextureCache():removeTextureForKey(M_LOADING_BG)
+	cc.Director:getInstance():getTextureCache():removeTextureForKey(M_PROGRESS_BG)
+	cc.Director:getInstance():getTextureCache():removeTextureForKey(M_PROGRESS)
+end
+
+local function onNodeEvent(event) --@return typeOrObject
+	if "exit" == event then
+	   onExit()
+	end
+end
+
+local function createLayer() --@return typeOrObject
+	init()
+	loadTexture()
+	
+	layer:registerScriptHandler(onNodeEvent)
+	return layer
+end
+
+function createLoadingScene() --@return typeOrObject
+	local scene = cc.Scene:create()
+	scene:addChild(createLayer())
+	return scene
+end
+
--- src/ResourceName.lua
+++ src/ResourceName.lua
@@ -5,3 +5,35 @@
 M_LOADING_BG = "loadingBg.png"
 M_PROGRESS_BG = "loadingProgress_bg.png"
 M_PROGRESS = "loadingProgress.png"
+
+--register layer
+M_REGISTER_BG = "register_bg.png"
+M_REGISTER_BTN_UP = "reg_cj_up.png"
+M_REGISTER_BTN_DOWN = "reg_cj_down.png"
+M_REGISTER_BOUNDER = "register_bounder.png"
+
+--
+
+--login view
+M_LOGIN_BG = "loginview/login_bg.png"
+M_LOGIN_BTN_UP = "loginview/denglu_up.png"
+M_LOGIN_BTN_DOWN = "loginview/denglu_down.png"
+M_CHUANGJIAN_UP = "loginview/chuangjian_up.png"
+M_CHUANGJIAN_DOWN = "loginview/chuangjian_down.png"
+
+--main view
+M_MAINVIEW_BG ="mainview/background.png"
+M_MVDRAG_BG ="mainview/drag_bg.png"
+M_KZBTN_UP ="mainview/kz_btn_up.png"
+M_KZBTN_DOWN ="mainview/kz_btn_down.png"
+M_TDBTN_UP ="mainview/td_btn_up.png"
+M_TDBTN_DOWN ="mainview/td_btn_down.png"
+M_QHBTN_UP ="mainview/qh_btn_up.png"
+M_QHBTN_DOWN ="mainview/qh_btn_down.png"
+M_FLBTN_UP ="mainview/fl_btn_up.png"
+M_FLBTN_DOWN ="mainview/fl_btn_down.png"
+
+
+
+
+
